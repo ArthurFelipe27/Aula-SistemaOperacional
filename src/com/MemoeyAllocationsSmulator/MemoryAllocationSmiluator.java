e5454514381ee3ce506361117e72e7c5168a6fab
Laboratorio <laboratorio@uniprojecao.edu.br>
2025-04-22 11:31:33 -0300

    Criamos o codigo

---
+++ src/com/MemoeyAllocationsSmulator/MemoryAllocationSmiluator.java
@@ -0,0 +1,17 @@
+package com.MemoeyAllocationsSmulator;
+
+public class MemoryAllocationSmiluator extends JFrame {
+	
+	private final JComboBox<String>
+		strategyComboBox;
+	private final DefaultListModel<String> 
+		processListModel;
+	private final java.util.List<MemoryBlock> 
+		memoryBlocks;
+	private final java.util.List<Process>
+		processes;
+	
+	private final JPanel memoryPanel;
+	
+	
+}
